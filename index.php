af042d2195ecd660c84f8d6d732a8329fa42e231
Gerardo GMV <gmv.gerardo@gmail.com>
2022-12-15 20:31:52 -0600

    inicio tuto php

--- index.php
+++ index.php
@@ -7,6 +7,12 @@
     <title>Document</title>
 </head>
 <body>
+    <ul>
+        <li><a href="#">Productos</a></li>
+        <li><a href="#">Usuarios</a></li>
+        <li><a href="#">Servicios</a></li>
+    </ul>
+    <input type="file" name = "foto">
     <h1>TEST GIT</h1>
 </body>
 </html>
